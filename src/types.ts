dc63bcadea0b733dfd60c9f638e32963245b4825
eeem77 <escobarmernestoe@gmail.com>
2024-01-04 11:11:55 -0400

    first commit kambu pizza web astro js

---
+++ src/types.ts
@@ -0,0 +1,49 @@
+export interface CompatibilityItem {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface FeatureItem {
+  description: string;
+  icon: string;
+  title: string;
+}
+
+export interface MenuItem {
+  description: string;
+  icon: string;
+  title: string;
+  pricem: string;
+  pricel: string;
+}
+
+export interface ChickenItem {
+  description: string;
+  icon: string;
+  title: string;
+  pricem: string;
+  pricel: string;
+}
+
+export interface PartyItem {
+  description: string;
+  icon: string;
+  title: string;
+  people: string;
+  time: string;
+  hours: string;
+  chargue: string;
+  obs: string;
+}
+
+export interface FooterLink {
+  description: string;
+  icon: string;
+  url: string;
+}
+
+export interface NavItem {
+  title: string;
+  url: string;
+}
